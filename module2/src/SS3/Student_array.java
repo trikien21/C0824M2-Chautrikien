6fa423b7a6986b1a41265aa6104ad5be49c41897
Tri Kien <trikien@Tris-MacBook-Pro-3.local>
2024-11-13 19:54:52 +0700

    update

--- module2/src/SS3/Student_array.java
+++ module2/src/SS3/Student_array.java
@@ -6,5 +6,17 @@ public class Student_array {
         Scanner scanner= new Scanner(System.in);
         System.out.print("Input a name's student: ");
         String input_name= scanner.nextLine();
+
+        boolean isExist = false;
+        for (int i=0; i<students.length; i++){
+            if (students[i].equals(input_name)){
+                System.out.println("Possition of the students in the list" +input_name+ " is: "+i);
+                isExist = true;
+                break;
+            }
+        }
+        if (!isExist){
+            System.out.println("Not found "+ input_name+ " in the list.");
+        }
     }
 }
